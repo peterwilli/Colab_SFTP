ce4a5a6887115057b70606bdb1147876298ef704
Peter Willemsen <peter@codebuffet.co>
2020-06-08 10:20:04 +0000

    Update init.sh

--- Colab/init.sh
+++ Colab/init.sh
@@ -4,7 +4,7 @@ init()
     mkdir -p /content/ssh
     apt-get install -y sshfs
     cat /dev/zero | ssh-keygen -b 4096 -q -N ""
-    echo "sshfs -C -B 256000000 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@\$1:/root -p 2222 /content/ssh\necho 'Mounted /content/ssh'" > /usr/bin/mount_colab_sftp
+    echo "sshfs -C -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@\$1:/root -p 2222 /content/ssh\necho 'Mounted /content/ssh'" > /usr/bin/mount_colab_sftp
     chmod a+x /usr/bin/mount_colab_sftp
 }
 init > /dev/null 2>&1
